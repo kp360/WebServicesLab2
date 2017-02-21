7ef95083455aa34a6c4922675a8ccfe6c2c2b56f
kp360 <knaph@uw.edu>
2017-02-21 01:04:33 -0800

    Data Services step 4

---
+++ app/src/main/java/knaph/uw/tacoma/edu/webserviceslab2/authenticate/SignInActivity.java
@@ -0,0 +1,15 @@
+package knaph.uw.tacoma.edu.webserviceslab2.authenticate;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import knaph.uw.tacoma.edu.webserviceslab2.R;
+
+public class SignInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+    }
+}
